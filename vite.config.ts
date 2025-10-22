98011ce03b80d421f77064fa7a2501dcc1befa50
EC6 <gracie@apotomelabs.com>
2025-10-22 10:56:36 -0600

    Add `vite-plugin-sitemap` for automated sitemap generation and configure dynamic routes for SEO enhancements.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import Sitemap from 'vite-plugin-sitemap'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+      Sitemap({
+          hostname: 'https://hacktrinidadforward.io/',
+          dynamicRoutes:[
+              '/',
+              '/register',
+              '/about'
+          ],
+      })
   ],
   resolve: {
     alias: {
